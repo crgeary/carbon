dbd4941b000603ba6343f007ccd0e2409908fd2c
Christopher Geary <hello@crgeary.com>
2021-02-16 18:19:09 +0000

    Chunk files to be read in source-filesystem

--- packages/carbon-source-filesystem/carbon.server.ts
+++ packages/carbon-source-filesystem/carbon.server.ts
@@ -3,35 +3,38 @@ import fs from 'fs-extra';
 import recursive from 'recursive-readdir';
 import mime from 'mime-types';
 import { createHash } from 'crypto';
+import { chunk } from 'lodash';
 
 import { HookParams } from '@crgeary/carbon';
 
 export const source = async ({ actions, plugin }: HookParams) => {
     const { options } = plugin;
 
-    let files = await recursive(options.path, options.ignore || []);
+    const createNodeFromPath = async (file: string) => {
+        const _path = path.parse(file);
+        const content = await fs.readFile(file, `utf8`);
+        actions.createNode({
+            id: actions.createNodeId(file, `@crgeary/carbon-source-filesystem`),
+            collection: options.collection || `default`,
+            file: {
+                extension: _path.ext.slice(1),
+                name: _path.name,
+                base: _path.base,
+                path: file,
+            },
+            __carbon: {
+                type: `FILE`,
+                mediaType: mime.lookup(_path.ext) || null,
+                content: content,
+                contentDigest: createHash('md5').update(content).digest('hex'),
+            },
+        });
+    };
 
-    await Promise.all(
-        files.map(async (file) => {
-            const _path = path.parse(file);
-            const content = await fs.readFile(file, `utf8`);
-            actions.createNode({
-                id: actions.createNodeId(file, `@crgeary/carbon-source-filesystem`),
+    const files = await recursive(options.path, options.ignore || []);
+    const chunks = chunk(files, 250);
 
-                collection: options.collection,
-                file: {
-                    extension: _path.ext.slice(1),
-                    name: _path.name,
-                    base: _path.base,
-                    path: file,
-                },
-                __carbon: {
-                    type: `FILE`,
-                    mediaType: mime.lookup(_path.ext) || null,
-                    content: content,
-                    contentDigest: createHash('md5').update(content).digest('hex'),
-                },
-            });
-        })
-    );
+    for (let i = 0; i < chunks.length; i++) {
+        await Promise.all(chunks[i].map(createNodeFromPath));
+    }
 };
